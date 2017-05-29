955e09d79af66609f4200cbc85d1c3086e22af0f
Hariprasanth <shhariprasanth@gmail.com>
2017-05-29 17:17:24 +0530

    layout modified in the .xml file for a good look

--- RGB/app/src/main/java/com/example/android/rgb/MainActivity.java
+++ RGB/app/src/main/java/com/example/android/rgb/MainActivity.java
@@ -6,19 +6,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ScrollView;
+import android.widget.LinearLayout;
+
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int r, g, b;
-    ScrollView view;
+    LinearLayout view;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        view = (ScrollView) findViewById(R.id.background);
+        view = (LinearLayout) findViewById(R.id.background);
         ColorDrawable colorDrawable = (ColorDrawable) view.getBackground();
         int color = colorDrawable.getColor();
         r = Color.red(color);

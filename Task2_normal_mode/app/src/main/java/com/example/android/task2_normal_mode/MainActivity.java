3f810e01c1eba690091614683e892a50ef0d3cf3
Hariprasanth <shhariprasanth@gmail.com>
2017-06-15 14:58:22 +0530

    caption textbox and button added

--- Task2_normal_mode/app/src/main/java/com/example/android/task2_normal_mode/MainActivity.java
+++ Task2_normal_mode/app/src/main/java/com/example/android/task2_normal_mode/MainActivity.java
@@ -10,7 +10,9 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -27,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
     ListView imagesList;
     Intent takePicture;
     Intent pickPhoto;
+    CharSequence colors[];
+    EditText captionTextbox;
+    String captionText;
+    Button captionButton;
+    Bitmap capturedImage;
+    Bitmap selectedImage;
+    int flagForIntentPhoto = 0;
+    int index = 0,flag = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,40 +48,60 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CharSequence colors[] = new CharSequence[] {"Camera", "Gallery"};
+                if(index == flag) {
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Pick a photo from");
-                builder.setItems(colors, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        if(which == 0) {
-                            takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                            startActivityForResult(takePicture, 0);
+                    colors = new CharSequence[]{"Camera", "Gallery"};
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Pick a photo from");
+                    builder.setItems(colors, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (which == 0) {
+                                flagForIntentPhoto = 0;
+                                takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                startActivityForResult(takePicture, flagForIntentPhoto);
+                            } else if (which == 1) {
+                                flagForIntentPhoto = 1;
+                                pickPhoto = new Intent(Intent.ACTION_PICK,
+                                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                startActivityForResult(pickPhoto, flagForIntentPhoto);
+                            }
                         }
-                        else if (which == 1)
-                        {
-                            pickPhoto = new Intent(Intent.ACTION_PICK,
-                                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                            startActivityForResult(pickPhoto , 1);
-                        }
-                    }
-                });
-                builder.show();
-
-
-
+                    });
+                    builder.show();
+                }
+                else Toast.makeText(getApplicationContext(),"First add the caption!",Toast.LENGTH_SHORT).show();
             }
         });
+        captionButton = (Button) findViewById(R.id.caption_button);
+        captionTextbox = (EditText) findViewById(R.id.caption_textbox);
+        captionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(flag == (index-1)) {
+                    captionText = captionTextbox.getText().toString();
+                    if(flagForIntentPhoto==0)
+                    imagesWithCaption.set(flag, new ImageWithCaption(capturedImage,captionText));
+                    else imagesWithCaption.set(flag, new ImageWithCaption(selectedImage,captionText));
+                    adaptor.notifyDataSetChanged();
+                    flag++;
+                    captionTextbox.getText().clear();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "First add the image!", Toast.LENGTH_SHORT).show();
+                    captionTextbox.getText().clear();
+                }
+            }
+        });
+}
 
-    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
         switch(requestCode) {
             case 0:
                 if(resultCode == RESULT_OK){
-                    Bitmap capturedImage = (Bitmap) imageReturnedIntent.getExtras().get("data");
+                    capturedImage = (Bitmap) imageReturnedIntent.getExtras().get("data");
                     ByteArrayOutputStream bytes = new ByteArrayOutputStream();
                     capturedImage.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
                     File destination = new File(Environment.getExternalStorageDirectory(),
@@ -87,14 +117,15 @@ public class MainActivity extends AppCompatActivity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    imagesWithCaption.add(new ImageWithCaption(capturedImage,"Image from camera"));
+                    imagesWithCaption.add(index,new ImageWithCaption(capturedImage,"Add caption"));
                     adaptor.notifyDataSetChanged();
+                    index++;
                 }
 
                 break;
             case 1:
                 if(resultCode == RESULT_OK){
-                    Bitmap selectedImage=null;
+                    selectedImage=null;
                     if (imageReturnedIntent != null) {
                         try {
                             selectedImage = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), imageReturnedIntent.getData());
@@ -102,14 +133,11 @@ public class MainActivity extends AppCompatActivity {
                             e.printStackTrace();
                         }
                     }
-                    imagesWithCaption.add(new ImageWithCaption(selectedImage,"Image from gallery"));
+                    imagesWithCaption.add(index,new ImageWithCaption(selectedImage,"Add caption"));
                     adaptor.notifyDataSetChanged();
+                    index++;
                 }
                 break;
         }
     }
-
-
-
-
 }
